3bc5983bbd86e0834adaa549b6b098d3dbb2f299
Fahri Dede <fahri.dede@belbim.istanbul>
2021-02-02 09:42:52 +0300

    Kamp 4. Gün

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrray Diziler
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            ////System.IndexOutOfRangeException hatası alırız.
+            ////Arrayleri oluştururken hangi sınırda oluşturduysak onun dışına çıkamayız.
+            //isimler[4] = "İlker";
+            ////isimler[4] = "İlker";
+            ////Arraylerde sabitlik var 4 elemanlı tanımladıysak eleman sayısını artıramıyoruz.
+            ////new leyerek artırım yaparız ama bu sefer de arraydeki önceki elemanları kaybederiz.
+            ////Bu kullanım kısıtlarından dolayı hem C#'da hem de Javada genellikle array kullanılmaz collectionslar List<> kullanılır.
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            
+            
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+        //void emir kipinde git ekle, git güncelle, git bişeyyap anlamında void tanımlarız.
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+
+    }
+}
