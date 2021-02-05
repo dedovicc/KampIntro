6875de85150c8d3e309e142654912a83bfa15dd9
Fahri Dede <fahri.dede@belbim.istanbul>
2021-02-05 14:52:19 +0300

    Kamp 4. gün 5. ödev gönderildi.

---
+++ Homework_4_5_MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_4_5_MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArrayKey = keys;
+
+            keys = new TKey[keys.Length + 1];
+            TValue[] tempArrayValue = values;
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+            }
+            keys[keys.Length - 1] = key;
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                values[i] = tempArrayValue[i];
+            }
+            values[values.Length - 1] = value;
+
+        }
+    }
+}
---
+++ Homework_4_5_MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework_4_5_MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(15, "Ahmet");
+            students.Add(15, "Mehmet");
+
+            
+        }
+    }
+}
