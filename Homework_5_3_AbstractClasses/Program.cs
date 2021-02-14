5bdd8d64fdcd28993723791723086a1f16e4a2e0
Fahri Dede <fahri.dede@belbim.istanbul>
2021-02-15 01:02:32 +0300

    Kamp 5. Gün, Ödev 3 Commit edildi.

---
+++ Homework_5_3_AbstractClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework_5_3_AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtendedClass extendedClass = new ExtendedClass();
+            extendedClass.Method1();
+            extendedClass.Method2();
+        }
+
+        
+    }
+
+    public abstract class BaseClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("abstract classın içerisindeki abstract olmayan method çalıştırıldı");
+        }
+        public abstract void Method2();
+    }
+
+    public class ExtendedClass : BaseClass
+    {
+        public override void Method2()
+        {
+            Console.WriteLine("Abstract classın içerisnde imzası bulunan Method2 ExtendedClassın içerisinde override edilerek çalıştırıldı.");
+        }
+    }
+}
